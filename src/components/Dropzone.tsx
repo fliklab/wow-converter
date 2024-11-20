f9742c55ae2a94bf9c45f577cb1800f98f095eb4
Flik Jeong <jsh852@gmail.com>
2024-11-21 04:36:21 +0900

    Create option to upload file mannually

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
 interface DropzoneProps {
   onDrop: (files: File[]) => void;
 }
 
 const Dropzone: React.FC<DropzoneProps> = ({ onDrop }) => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
     const files = Array.from(event.dataTransfer.files);
     onDrop(files);
+  };
+
+  const handleFileInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const files = Array.from(event.target.files);
+      onDrop(files);
+    }
+  };
+
+  const handleClick = () => {
+    fileInputRef.current?.click();
   };
 
   return (
@@ -16,8 +29,17 @@ const Dropzone: React.FC<DropzoneProps> = ({ onDrop }) => {
       className="dropzone"
       onDragOver={(e) => e.preventDefault()}
       onDrop={handleDrop}
+      onClick={handleClick}
     >
-      <p>Drag and drop your images here</p>
+      <input
+        type="file"
+        ref={fileInputRef}
+        onChange={handleFileInput}
+        multiple
+        accept="image/jpeg,image/png,image/webp"
+        style={{ display: "none" }}
+      />
+      <p>이미지를 드래그하거나 클릭하여 업로드하세요</p>
     </div>
   );
 };
